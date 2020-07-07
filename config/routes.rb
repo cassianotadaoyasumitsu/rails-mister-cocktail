b2b0157e14e5ea03725c5815fa7b270e1bd510de
cassiano yasumitsu <cassiano@usuarioincomum.com>
2020-07-07 20:16:14 +0900

    reroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails do
+  root to: 'cocktails#index'
+
+  resources :cocktails, except: :index do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
